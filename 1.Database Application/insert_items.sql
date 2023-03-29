c581d129403f398126997f4269d4eb9865240f81
Chengqian Zhang <20000110006@stu.pku.edu.cn>
2023-03-29 21:31:04 +0800

    merge insert to one file

--- 1.Database Application/insert_items.sql
+++ 1.Database Application/insert_items.sql
@@ -216,3 +216,126 @@ INSERT INTO song_belongs_to_song_list_tb (
     3,
     3
 );
+
+-- 9. 创建动态表。
+
+INSERT INTO moment_tb (
+    content, 
+    range_of_share, 
+    id_of_the_moment_creating_user,
+) VALUES (
+    '今天去家园食堂吃了鸡腿饭', '粉丝可见', 1
+);
+
+INSERT INTO moment_tb (
+    content, 
+    range_of_share, 
+    id_of_the_moment_creating_user,
+) VALUES (
+    '2023考研冲冲冲', '自己可见', 1
+);
+
+INSERT INTO moment_tb (
+    content, 
+    id_of_the_moment_creating_user,
+) VALUES (
+    '许嵩又出新专辑', 3
+);
+
+-- 10. 创建评论表。
+
+INSERT INTO comment_tb (
+    content,
+    belong_moment_id,
+    id_of_the_comment_user,
+) VALUES (
+    '家园的鸡排饭也很不错',
+    1,
+    1
+);
+
+INSERT INTO comment_tb (
+    content,
+    belong_moment_id,
+    id_of_the_comment_user,
+) VALUES (
+    '考研加油',
+    2,
+    2
+);
+
+INSERT INTO comment_tb (
+    content,
+    belong_moment_id,
+    id_of_the_comment_user,
+) VALUES (
+    '许嵩粉丝报道',
+    3,
+    3
+);
+
+-- 11. 创建点赞表。
+
+INSERT INTO liked_tb (
+    belong_moment_id,
+    id_of_the_liked_user,
+) VALUES (
+    1,
+    2
+);
+
+INSERT INTO liked_tb (
+    belong_moment_id,
+    id_of_the_liked_user,
+) VALUES (
+    2,
+    3
+);
+
+INSERT INTO liked_tb (
+    belong_moment_id,
+    id_of_the_liked_user,
+) VALUES (
+    3,
+    1
+);
+
+INSERT INTO liked_tb (
+    belong_moment_id,
+    id_of_the_liked_user,
+) VALUES (
+    2,
+    2
+);
+
+-- 12. 创建转发表。
+
+INSERT INTO trans_tb (
+    content,
+    belong_moment_id,
+    id_of_the_trans_user,
+) VALUES (
+    '我们也去家园吃饭吧',
+    1,
+    2
+);
+
+INSERT INTO trans_tb (
+    content,
+    belong_moment_id,
+    id_of_the_trans_user,
+) VALUES (
+    '看看当代大学生内卷的现状吧',
+    2,
+    3
+);
+
+INSERT INTO trans_tb (
+    content,
+    belong_moment_id,
+    id_of_the_trans_user,
+) VALUES (
+    '姐妹们我买好票了',
+    3,
+    1
+);
