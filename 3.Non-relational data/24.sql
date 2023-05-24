7c7feb2b3bc33dd4c4a822f01b284337ea5041e6
XierLabber <623087961@qq.com>
2023-05-24 23:59:52 +0800

    修改了markdown

--- 3.Non-relational data/24.sql
+++ 3.Non-relational data/24.sql
@@ -1,4 +1,4 @@
 SELECT AVG(val) OVER (ORDER BY date ROWS BETWEEN 0 PRECEDING AND 4 FOLLOWING) AS result FROM
 (
-	SELECT close - lead(close, 5) OVER (ORDER BY trade_date) AS val, trade_date AS date FROM tushare
+	SELECT close - LAG(close, 5) OVER (ORDER BY trade_date) AS val, trade_date AS date FROM tushare
 ) AS tmp
